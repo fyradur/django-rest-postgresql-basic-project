e1d50aee23c462e7c1c3747091903f9ff9a5f23e
John Möller <john.moller@outlook.com>
2023-03-19 15:06:49 +0100

    Added section on testing it out locally

--- project/customer/urls.py
+++ project/customer/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', CustomerList.as_view()),
     path('<int:pk>/', CustomerDetail.as_view(), name='retrieve-customer'),
     path('update/<int:pk>/', CustomerUpdate.as_view(), name='update-customer'),
-    # path('delete/<int:pk>/', CustomerDelete.as_view(), name='delete-customer')
+    path('delete/<int:pk>/', CustomerDelete.as_view(), name='delete-customer')
 ]
